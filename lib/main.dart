56bdb1d7eee5e98a30f19ee9d6692169fd5ae4cd
Raxnakum11 <148608416+Raxnakum11@users.noreply.github.com>
2025-03-05 10:41:48 +0530

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,7 @@ class MyApp extends StatelessWidget {
         'faculty_home': (context) => const FacultyHome(),
         'student_profile': (context) => const StudentProfile(),
         'faculty_profile': (context) => const FacultyProfile(),
-      },
+      }, /////////////////////////////////
     );
   }
 }
