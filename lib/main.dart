5fdf0eee472053245de6dc4c9f777816b52c9462
Raxnakum11 <148608416+Raxnakum11@users.noreply.github.com>
2025-03-05 10:53:03 +0530

    change in main.dart code

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,20 @@
 // }
 
 //import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/splash.dart';
+// import 'package:flutter_application_1/student_profile.dart';
+// import 'package:flutter_application_1/faculty_profile.dart';
+// import 'package:flutter_application_1/register.dart';
+// import 'package:flutter_application_1/login.dart';
+// import 'package:flutter_application_1/faculty_home.dart';
+// import 'package:flutter_application_1/student_home.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/splash.dart';
 import 'package:flutter_application_1/student_profile.dart';
@@ -42,12 +56,6 @@ import 'package:flutter_application_1/register.dart';
 import 'package:flutter_application_1/login.dart';
 import 'package:flutter_application_1/faculty_home.dart';
 import 'package:flutter_application_1/student_home.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(const MyApp());
-// }
 
 void main() {
   runApp(const MyApp());
@@ -70,7 +78,7 @@ class MyApp extends StatelessWidget {
         'faculty_home': (context) => const FacultyHome(),
         'student_profile': (context) => const StudentProfile(),
         'faculty_profile': (context) => const FacultyProfile(),
-      }, /////////////////////////////////
+      },
     );
   }
 }
